401b4d810d833a66f60fc36cea6d8dd5b4b68e00
kohllisti <kohllisti@riseup.net>
2015-11-13 16:45:30 +0100

    Feature - added API v1

--- maps/v1_views.py
+++ maps/v1_views.py
@@ -2,10 +2,20 @@
 from rest_framework import viewsets
 
 # ViewSets define the view behavior.
-from maps.models import Map
-from maps.v1_serializers import MapSerializer
+from maps.models import Map, Feature
+from maps.v1_serializers import MapSerializer, FeatureSerializer
 
 
 class MapViewSet(viewsets.ModelViewSet):
     queryset = Map.objects.public()
     serializer_class = MapSerializer
+
+
+class FeatureViewSet(viewsets.ModelViewSet):
+    queryset = Feature.objects.all()
+    serializer_class = FeatureSerializer
+
+    def get_queryset(self):
+        qs = super(FeatureViewSet, self).get_queryset()
+        map_name = self.kwargs.get('map_name', '')
+        return qs.filter(map__name=map_name)
