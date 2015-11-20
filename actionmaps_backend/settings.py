04b95b7da019deac16d8ba1a92ce29b4657a6461
kohllisti <kohllisti@riseup.net>
2015-11-20 23:04:32 +0100

    CORS - add cors middleware

--- actionmaps_backend/settings.py
+++ actionmaps_backend/settings.py
@@ -42,10 +42,13 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework_gis',
 
+    'corsheaders',
     'maps',
 )
 
 MIDDLEWARE_CLASSES = (
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,3 +123,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+
+# CORS
+if DEBUG is True:
+    CORS_ORIGIN_ALLOW_ALL = True
+else:
+    CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    # 'example.com'
+)
