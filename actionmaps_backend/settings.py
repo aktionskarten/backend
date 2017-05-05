a361edede5796af498a3316bd45c1cc82c1080dd
iom <iom@riseup.net>
2017-05-05 21:25:20 +0200

    encourage users to generate SECRET_KEY

--- actionmaps_backend/settings.py
+++ actionmaps_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$a!gq4l#+5=ny7j-nu-l5yjojb49y=j4g7ru+cxdznks3gvz($'
+SECRET_KEY = 'YOUR_SECRET_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
