ef22e0ee39aacccf1a41349e0ef7f62fa7364f27
iom <iom@riseup.net>
2017-11-01 17:12:41 +0100

    Make app global to be mountable through uwsgi

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
 
+app = create_app()
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
