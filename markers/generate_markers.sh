d71a07c9e78b3954900e637fa2481e1890400c5e
iom <iom@riseup.net>
2017-11-01 16:39:52 +0100

    Add marker generation util
    To generate markers, you now can use generate_markers.sh. It will
    download a spreadsheet of markers and generate a single image for each
    marker. You need imagemagick installed for this.

---
+++ markers/generate_markers.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+URL="https://github.com/KartographischeAktion/AktionskartenMarker/raw/gh-pages/AktionskartenMarker.png"
+INPUT="INPUT.png"
+SIZE=150
+COLORS=(e04f9e fe0000 ee9c00 ffff00 00e13c 00a54c 00adf0 7e55fc 1f4199 7d3411)
+NAMES=(train megaphone tent speaker reheat cooking-pot police nuclear empty point information exclamation-mark star star-megaphone arrow bang)
+NAMES_ALT=(flag megaphone empty point exclamation-mark thor-steinar arrow)
+
+wget -O ${INPUT} ${URL}
+
+for (( i=0; i<${#COLORS[@]}; i++ )); do
+  color=\#${COLORS[i]}
+  mkdir -p $color
+  echo $color
+
+  names=("${NAMES[@]}")
+  if [[ "i" -ge "$((${#COLORS[@]}-1))" ]]; then
+    names=("${NAMES_ALT[@]}")
+  fi
+
+  for (( j=0; j<${#names[@]}; j++ )); do
+    filename=${color}/${names[j]}.png
+    offset="$(($j*$SIZE))+$(($i*$SIZE))"
+    convert ${INPUT} -crop ${SIZE}x${SIZE}+${offset} ${filename}
+    echo -e "\t* ${color}/${names[j]}.png"
+  done
+done
+
+rm ${INPUT}
