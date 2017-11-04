18186405b89ae4cc1cf01f4403cb9b40e20c4bd4
Jonatan Zint <j.zint@frykk.de>
2017-11-04 13:18:00 +0100

    make the dockerfile great again

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,4 +3,5 @@ cd /source
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export PYTHONPATH=${PYTHONPATH}:.
+/source/markers/generate_markers.sh
 FLASK_APP=src/app.py FLASK_DEBUG=1 flask run --host=0.0.0.0
