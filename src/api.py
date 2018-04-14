6d45986b4588d223056a4fb8703215bbc69092c2
iom <iom@riseup.net>
2018-04-14 12:06:42 +0200

    Rework api
    * add support for map attributes
    * add grid endpoint
    * use refactored Model interface

--- src/api.py
+++ src/api.py
@@ -1,10 +1,12 @@
 import re
 
-from flask import Blueprint, request, jsonify, abort
+from flask import Blueprint, request, jsonify, abort, redirect, url_for
 from flask_cors import CORS
 from werkzeug.security import safe_str_cmp
 from models import db, Map, Feature
+from grid import Grid
 from geojson import FeatureCollection
+from datetime import datetime
 
 
 api = Blueprint('API', __name__)
@@ -14,7 +16,7 @@ CORS(api)
 @api.route('/api/maps/<string:map_id>/auth')
 def auth(map_id):
     secret = request.headers.get('X-Secret', '')
-    obj = db.session.query(Map).get(map_id)
+    obj = Map.get(map_id)
 
     if not obj or not secret:
         abort(400)  # bad request - invalid input
@@ -32,7 +34,7 @@ def verify_token():
     if not map_id or not token:
         abort(400)
 
-    obj = db.session.query(Map).get(map_id)
+    obj = Map.get(map_id)
     if not obj or not obj.check_token(token):
         abort(401)
 
@@ -41,30 +43,73 @@ def verify_token():
 @api.route('/api/maps', methods=['GET', 'POST'])
 def maps():
     if request.method == 'POST':
-        if ('bbox' not in request.json):
+        if ('name' not in request.json):
             abort(400)
 
-        m = Map(request.json['bbox'])
-        if 'name' in request.json:
-            m.name = request.json['name']
+        name = request.json['name']
+
+        m = Map.get(name)
+        if (m):
+            abort(400)
+
+        m = Map(name)
+
+        for key in ['name', 'bbox', 'description', 'place']:
+            if key in request.json:
+                setattr(m, key, request.json[key])
+
+        if 'date' in request.json and 'time' in request.json:
+            date_str = "{} {}".format(request.json['date'], request.json['time'])
+            m.datetime = datetime.strptime(date_str, '%Y-%m-%d %H:%M')
+
+        if 'keys' in request.json and 'values' in request.json:
+            keys = request.json['keys']
+            values = request.json['values']
+            if len(keys) == len(values):
+                m.attributes = dict(zip(keys, values))
+
         db.session.add(m)
         db.session.commit()
-        return jsonify(m.to_dict())
-    return jsonify([m.to_dict() for m in db.session.query(Map).all()])
+
+        return redirect(url_for('.map_get', code=303, map_id=m.slug))
+
+    return jsonify([m.to_dict() for m in Map.all()])
 
 
-@api.route('/api/maps/<string:map_id>/')
-@api.route('/api/maps/<string:map_id>')
+@api.route('/api/maps/<string:map_id>/', methods=['GET', 'PATCH'])
+@api.route('/api/maps/<string:map_id>', methods=['GET', 'PATCH'])
 def map_get(map_id):
-    m = db.session.query(Map).get(map_id)
+    m = Map.get(map_id)
+
     if not m:
         abort(404)
+
+    if request.method == 'PATCH':
+        for key in ['name', 'bbox', 'description', 'place']:
+            if key in request.json:
+                setattr(m, key, request.json[key])
+
+        if 'date' in request.json and 'time' in request.json:
+            date_str = "{} {}".format(request.json['date'], request.json['time'])
+            m.datetime = datetime.strptime(date_str, '%Y-%m-%d %H:%M')
+
+        if 'keys' in request.json and 'values' in request.json:
+            keys = request.json['keys']
+            values = request.json['values']
+            if len(keys) == len(values):
+                m.attributes = dict(zip(keys, values))
+        else:
+            m.attributes = {}
+
+        db.session.add(m)
+        db.session.commit()
+
     return jsonify(m.to_dict())
 
 
 @api.route('/api/maps/<string:map_id>/features', methods=['GET', 'POST'])
 def map_features(map_id):
-    m = db.session.query(Map).get(map_id)
+    m = Map.get(map_id)
     if not m:
         abort(404)
 
@@ -85,7 +130,7 @@ def map_features(map_id):
 @api.route('/api/maps/<string:map_id>/features/<int:feature_id>',
            methods=['PUT', 'PATCH', 'DELETE'])
 def map_feature_get(map_id, feature_id):
-    f = db.session.query(Feature).get(feature_id)
+    f = Feature.get(feature_id)
     if not f:
         abort(404)
 
@@ -97,29 +142,32 @@ def map_feature_get(map_id, feature_id):
     if not request.json:
         abort(400)
 
-    if request.method == 'PATCH':
-        if 'style' in request.json:
-            style = request.json['style']
-            if 'iconColor' in style:
-                # ensure that we deal with hex values (not rgb(r,g,b))
-                if style['iconColor'].startswith('rgb'):
-                    rgb = re.findall(r'\d+', style['iconColor'])
-                    iconColor = ''.join([('%02x' % int(x)) for x in rgb])
-                    style['iconColor'] = '#' + iconColor
-            f.style = style
-        if 'geo' in request.json:
-            f.geo = request.json['geo']
-    else:
-        f.geo = request.json
+    # it's PUT or PATCH now
+    if 'properties' in request.json:
+        props = request.json['properties']
+        if 'iconColor' in props:
+            # ensure that we deal with hex values (not rgb(r,g,b))
+            if props['iconColor'].startswith('rgb'):
+                rgb = re.findall(r'\d+', props['iconColor'])
+                iconColor = ''.join([('%02x' % int(x)) for x in rgb])
+                props['iconColor'] = '#' + iconColor
+        f.style = props
+
+    if 'geometry' in request.json:
+        f.geo = request.json['geometry']
 
     db.session.add(f)
     db.session.commit()
+
     return jsonify(f.to_dict())
 
+@api.route('/api/grid/<string:bbox>')
+def grid_get(bbox):
+    return jsonify(Grid(*map(float, bbox.split(','))).generate())
 
 @api.route('/api/maps/<string:map_id>/grid')
 def map_get_grid(map_id):
-    m = db.session.query(Map).get(map_id)
+    m = Map.get(map_id)
     if not m:
         abort(404)
     return jsonify(m.grid)
