8c35e450a506bb38daf4475d2cb8b9bf905d8230
iom <iom@riseup.net>
2018-05-02 01:31:47 +0200

    Update default settings

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 SECRET_KEY = "GENERATE SUPER SECRET KEY"
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost/test_geoalchemy2"
+SQLALCHEMY_DATABASE_URI = "postgresql://gis:gis@localhost/maps"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-MAPNIK_OSM_XML = "../mapnik-stylesheets/osm.xml"
-
+MAPNIK_OSM_XML = "openstreetmap-carto/osm.xml"
+NO_MAP_CACHE = False
