1ad80ab7ea1906974b179e954f4256fc4f17b668
iom <iom@riseup.net>
2019-04-17 18:28:13 +0200

    settings_default - remove unused variables

--- app/settings_default.py
+++ app/settings_default.py
@@ -1,7 +1,3 @@
 SECRET_KEY = "GENERATE SUPER SECRET KEY"
 DEBUG = False
 MAPNIK_OSM_XML = "libs/openstreetmap-carto/style.xml"
-NO_MAP_CACHE = True
-CELERY_BROKER_URL='redis://localhost:6379',
-CELERY_RESULT_BACKEND='redis://localhost:6379'
-
