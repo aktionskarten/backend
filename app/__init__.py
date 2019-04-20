900aa5fe4d8e57d8af4ca27817241f6de7d6bbe7
iom <iom@riseup.net>
2019-04-20 19:16:08 +0200

    Specify app through flaskenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,28 @@
 from flask import Flask
 from os import environ
 
+import rq
+from redis import Redis
+
 #from live import socketio
-from api import api
+from app.api import api
+from render import render
 from models import db
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object('settings_default')
+    app.config.from_object('app.settings_default')
     if 'SETTINGS' in environ:
         app.config.from_envvar('SETTINGS')
 
     db.init_app(app)
-
     #socketio.init_app(app)
 
-    for blueprint in [api]:
+    app.task_queue = rq.Queue(connection=Redis())
+
+    for blueprint in [render, api]:
         app.register_blueprint(blueprint)
 
     return app
