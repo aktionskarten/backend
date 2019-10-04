fe6d1a957ad24504e438415cb5726b3c063c885f
iom <iom@riseup.net>
2019-10-04 13:10:02 +0200

    Grid - support different styles

--- app/grid.py
+++ app/grid.py
@@ -2,14 +2,21 @@ from geojson import FeatureCollection, Feature, MultiLineString, LineString
 from numpy import linspace
 from string import ascii_uppercase
 
+STYLES = {
+    'red': (("#000000", "#ffffff"), ('#FF0000', '#000000')),
+    'green': (("#000000", "#ffffff"), ('#00FF00', '#000000')),
+    'violet': (("#000000", "#ffffff"), ('#8A2BE2', '#ffffff')),
+    'blue': (("#ffffff", "#000000"), ('#00b1f0', '#ffffff'))
+}
 
 class Grid:
 
-    def __init__(self, min_x, min_y, max_x, max_y):
+    def __init__(self, min_x, min_y, max_x, max_y, style='red'):
         self.min_x = min_x
         self.max_x = max_x
         self.min_y = min_y
         self.max_y = max_y
+        self.style = style
 
     def generate(self, cells = 12):
         features = []
@@ -25,8 +32,8 @@ class Grid:
 
             data = {
                 'label': ascii_uppercase[i],
-                'color': "#000000" if i % 2 else "#FF0000",
-                'labelColor': "#FFFFFF" if i % 2 else "#000000"
+                'color': STYLES[self.style][i%2][0],
+                'labelColor': STYLES[self.style][i%2][1]
             }
             for j, geo in enumerate(geos):
                 data['pos'] = 'TOP' if j % 2 else 'BOTTOM'
@@ -45,8 +52,8 @@ class Grid:
 
             data = {
                 'label': len(entries) - i - 1,
-                'color': "#000000" if i % 2 else "#FF0000",
-                'labelColor': "#FFFFFF" if i % 2 else "#000000"
+                'color': STYLES[self.style][i%2][0],
+                'labelColor': STYLES[self.style][i%2][1]
             }
             for j, geo in enumerate(geos):
                 data['pos'] = 'RIGHT' if j % 2 else 'LEFT'
