1a8a3e5f7464f0029714b42ebd2818e8ca420b80
iom <iom@riseup.net>
2019-10-16 19:07:06 +0200

    cli/pymapnik - add include path and error checks

--- app/cli/pymapnik.py
+++ app/cli/pymapnik.py
@@ -1,4 +1,5 @@
 import click
+import sys
 
 from distutils.core import run_setup
 from os import environ
@@ -29,13 +30,26 @@ def is_installed():
 @pymapnik.command()
 def install():
     if _mapnik_is_installed():
+        click.secho("Mapnik already installed!", fg='green')
         return
 
     environ['PYCAIRO'] = "true"
     with Path("libs/python-mapnik"):
         import subprocess
-        subprocess.call(['python', 'setup.py', 'clean'])
-        subprocess.call(['python', 'setup.py', 'install'])
+        import cairo
+        child = subprocess.call(['python', 'setup.py', 'clean'])
+        if child != 0:
+            sys.exit(-1)
+
+        include_path = '--include-dirs=' + cairo.get_include()
+        child = subprocess.call(['python', 'setup.py', 'build_ext', include_path])
+        if child != 0:
+            sys.exit(-1)
+
+        child = subprocess.call(['python', 'setup.py', 'install'])
+        if child != 0:
+            sys.exit(-1)
+
         # following seems not to work in debian 9
         #dist = run_setup('setup.py')
         #dist.run_command('clean')
