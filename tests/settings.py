4d3d210cc77fa6aacc8e3af38592177f9657ac4e
iom <iom@riseup.net>
2019-10-22 20:00:49 +0200

    cli/pymapnik - Remove unused code

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,5 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://test:test@127.0.0.1:5432/maps"
+SQLALCHEMY_DATABASE_URI = "postgresql://test:test@127.0.0.1:5432/test"
 DB_HOST = '127.0.0.1'
 DB_USER = 'test'
 DB_PASS = 'test'
-DB_NAME = 'maps'
+DB_NAME = 'test'
