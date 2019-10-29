1aa133f9d4b9fb50d9afe78895fb41f5e85808b1
waschbeer <waschbeer@riseup.net>
2019-10-29 23:45:25 +0100

    Add basic Dockerfile for backend
    [WIP] Update docker-compose.yml and docker-entrypoint.sh

--- deploy/docker/docker-entrypoint.sh
+++ deploy/docker/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /source
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-/source/markers/generate_markers.sh
-FLASK_DEBUG=1 FLASK_APP=src/app.py flask run --host=0.0.0.0
+
+flask postgres init
+flask osm init
+
+flask run --with-threads --no-reload --host=0.0.0.0
