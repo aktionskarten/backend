9c5b9f0a2bb30fdefa03530a468a7de9f622e4b2
iom <iom@riseup.net>
2019-11-29 16:39:03 +0100

    use new configs

---
+++ app/settings.py
@@ -0,0 +1,36 @@
+class DefaultConfig(object):
+    """Base config, uses staging database server."""
+    DEBUG = False
+    TESTING = False
+
+    MAPNIK_OSM_XML = "libs/openstreetmap-carto/style.xml"
+    SECRET_KEY = "GENERATE SUPER SECRET KEY"
+
+    DB_HOST = 'localhost'
+    DB_USER = 'maps'
+    DB_PASS = 'maps'
+    DB_NAME = 'maps'
+
+    OSM_DB_HOST = 'localhost'
+    OSM_DB_USER = 'maps'
+    OSM_DB_PASS = 'maps'
+    OSM_DB_NAME = 'osm'
+
+    REDIS_HOST = 'localhost'
+
+    # needed for events
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        return 'postgres://{}:{}@{}/{}'.format(self.DB_USER, self.DB_PASS,
+                                               self.DB_HOST, self.DB_NAME)
+
+class DevelopmentConfig(DefaultConfig):
+    DEBUG = True
+
+class TestingConfig(DevelopmentConfig):
+    TESTING = True
+    DB_HOST = 'db'
+    OSM_DB_HOST = 'db'
+    REDIS_HOST = 'redis'
