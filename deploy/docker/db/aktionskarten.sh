51bef0d890b9b2143c03153bc5b83e9db44358a1
iom <iom@riseup.net>
2021-10-12 22:29:46 +0200

    Update docker/db files

--- deploy/docker/db/aktionskarten.sh
+++ deploy/docker/db/aktionskarten.sh
@@ -3,7 +3,3 @@
 echo "Init postgres for aktionskarten"
 python postgres.py createuser --user maps --password maps
 python postgres.py initdb --owner maps maps
-
-echo "Init osm for aktionskarten"
-python osm.py initdb --user maps --password maps --name osm
-python osm.py import-dump --name osm /source/data/kotti-latest.osm.pbf
